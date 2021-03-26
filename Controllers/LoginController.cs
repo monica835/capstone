61b4f70d69ef699366ed0aab2bbef8ab71653ebf
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2021-03-26 08:50:16 +0800

    commit

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Icarus.Models;
 
 namespace Icarus.Controllers
 {
     public class LoginController : Controller
     {
+        private ICARUSDBEntities db = new ICARUSDBEntities();
+
         [Route("/login")]
         // GET: Login
         public ActionResult Index()
@@ -15,9 +18,26 @@ namespace Icarus.Controllers
             return View();
         }
 
+        [Route("/Login")]
         public ActionResult Login()
         {
             return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Login(tblStaff user)
+        {
+            var checkLogin = db.tblStaffs.Where(x => x.Username.Equals(user.Username) && x.Password.Equals(user.Password)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                Session["IDStaff"] = user.IDStaff.ToString();
+                Session["Username"] = user.Username.ToString();
+                return RedirectToAction("Index", "Resident");
+            }
+            else {
+                ViewBag.Notification = "Username or Password doesn't match!";
+            }
+            return View();
         }
     }
 }
